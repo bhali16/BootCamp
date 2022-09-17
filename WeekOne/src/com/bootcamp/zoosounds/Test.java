3eed64cc93f6eea0a95f5146b619fb348d9b9b1f
Waqar Ahmad Khan <waqarbinmuhammad@gmail.com>
2022-09-17 21:42:48 +0500

    Create Test.java

---
+++ WeekOne/src/com/bootcamp/zoosounds/Test.java
@@ -0,0 +1,11 @@
+package com.bootcamp.zoosounds;
+
+public class Test {
+    public static void main(String[] args) {
+        Cat c1 = new Cat();
+        c1.Sound();
+
+        Dog d1 = new Dog();
+        d1.Sound();
+    }
+}
